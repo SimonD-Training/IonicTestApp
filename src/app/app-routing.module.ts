1ecc1336654aff67cced6b6a564336d34175cf77
SimonDMaxwell <lolicongress@outlook.com>
2022-08-18 17:55:18 -0500

    Empty pages and page routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core'
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router'
+import { CategoriesComponent } from './pages/categories/categories.component'
+import { ListComponent } from './pages/list/list.component'
 
 const routes: Routes = [
 	// {
 	//   path: 'home',
 	//   loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
 	// },
+  {
+    path: 'home',
+    component: ListComponent
+  },
+  {
+    path: 'list',
+    component: CategoriesComponent
+  },
 	{
 		path: '',
 		redirectTo: 'home',
